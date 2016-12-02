02ca72f0967994d11278797975392f25551a5a31
Mesbah Mowlavi <m@ionx.ca>
2016-12-01 21:57:54 -0800

    Made fancier!

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var assert = require('assert')
 var http = require('http');
  
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end('Hello World\n')
-}).listen(1337, '127.0.0.1')
- 
+var server = http.createServer(function (req, res) {
+  if (req.headers['content-type'] == 'application/json') {
+    res.writeHead(403, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({ error: "Our api is off limits!" }));
+    } else { res.writeHead(200, { 'Content-Type': 'text/plain'})
+      res.end('Greetings!\n')
+  }
+})
+
+server.listen(1337, '127.0.0.1')
 console.log('Server running at http://127.0.0.1:1337/')
